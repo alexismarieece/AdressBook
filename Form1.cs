7c957525c7f4950d04d8f2a48d05b11c1eccad35
Alexis MARIE <alexismarie2@live.fr>
2022-06-06 19:08:17 +0200

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdressBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("AdressBook v1.0\nAuthor: <Alexis MARIE>", "About");
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database1DataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: cette ligne de code charge les données dans la table 'database1DataSet.People'. Vous pouvez la déplacer ou la supprimer selon les besoins.
+            this.peopleTableAdapter.Fill(this.database1DataSet.People);
+
+        }
+    }
+}
